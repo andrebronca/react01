171c72109c6a247d5fedc881ad003d3647729f8b
Andre Bronca <bronca.andre@gmail.com>
2018-05-15 21:00:46 -0300

    Criando um componente

---
+++ src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends Component {
+    render(){
+        return (
+            <div className="my-class-from-react">
+                <h1>Estudo de React.js</h1>
+            </div>
+        )
+    }
+}
+
+export default HelloWorld
--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+import HelloWorld from './HelloWorld'
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById("root")
+)
