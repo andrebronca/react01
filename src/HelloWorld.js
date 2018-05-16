34ba3372fabb9eb4dde2b721041229a923317de0
Andre Bronca <bronca.andre@gmail.com>
2018-05-15 21:12:14 -0300

    adicionando estilo

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 class HelloWorld extends Component {
     render(){
+        const result = 4
+        const style = {
+            color: '#ff0033',
+            fontSize: 50,
+            backgroundColor: '#00f'
+        }
         return (
             <div className="my-class-from-react">
+                {/* Componente do react */}
                 <h1>Estudo de React.js</h1>
+                <h4 style={style}>{ result === 4 ? 'true' : 'false'}</h4>
             </div>
         )
     }
