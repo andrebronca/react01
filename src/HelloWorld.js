77bb902b0f9f2eaa73afeaad89d02becab9612b0
Andre Bronca <bronca.andre@gmail.com>
2018-05-15 21:26:25 -0300

    state

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
+import StateInput from './StateInput'
 
 class HelloWorld extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            name: 'Andre R.'
+        }
+
+    }
+
     render(){
         const result = 4
         const style = {
@@ -11,8 +21,10 @@ class HelloWorld extends Component {
         return (
             <div className="my-class-from-react">
                 {/* Componente do react */}
+                <StateInput />
                 <h1>Estudo de React.js</h1>
                 <h4 style={style}>{ result === 4 ? 'true' : 'false'}</h4>
+                <span>{this.state.name}</span>
             </div>
         )
     }
